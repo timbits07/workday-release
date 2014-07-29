fd3a5cd83c56b3a16d1e734026571cead0a93420
Tim Storey <tim.storey@yahoo.com>
2014-07-29 15:47:16 -0700

    initial commit to github

--- script/script.js
+++ script/script.js
@@ -1,17 +1,40 @@
-$(document).ready(function(){
+// Authenticate via API Key
+var tumblr = require('tumblr.js');
+var client = tumblr.createClient({ consumer_key: 'QLsxt7OS0goMSjkvah9Xys4dHacGDArTyPZ3merghVWrp03YtH' });
 
-var tumblrAPI = "http://api.tumblr.com/v2/blog/theworkdayrelease.tumblr.com/posts?api_key=QLsxt7OS0goMSjkvah9Xys4dHacGDArTyPZ3merghVWrp03YtH"
+// Make the request
+client.posts('theworkdayrelease.tumblr.com', { limit: 3, filter: 'html' }, function (err, data) {
+    console.log(client.posts);
+});
 
-$.getJSON( tumblrAPI, function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+
+// var tumblrAPI = "http://api.tumblr.com/v2/blog/theworkdayrelease.tumblr.com/posts?api_key=QLsxt7OS0goMSjkvah9Xys4dHacGDArTyPZ3merghVWrp03YtH"
+
+// $.getJSON( tumblrAPI, function( data ) {
+//   var items = [];
+//   $.each( data, function( key, val ) {
+//     items.push( "<li id='" + key + "'>" + val + "</li>" );
+//   });
  
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
+//   $( "<ul/>", {
+//     "class": "my-new-list",
+//     html: items.join( "" )
+//   }).appendTo( "body" );
+// });
 
-});
+// });
